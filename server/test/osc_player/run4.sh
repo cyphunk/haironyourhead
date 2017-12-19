132843259f8c9e5ef44b04310e584df679135c78
Nathan Fain <cyphunk@gmail.com>
2017-12-19 10:45:30 +0100

    .

--- server/test/osc_player/run4.sh
+++ server/test/osc_player/run4.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 timeout 20m python3 osc_record_replay.py ./nathan.csv 9999 replay localhost &
+sleep 25
 timeout 20m python3 osc_record_replay.py ./christiane.csv 9999 replay localhost &
+sleep 4
 timeout 20m python3 osc_record_replay.py ./chris.csv 9999 replay localhost &
+sleep 10
 timeout 20m python3 osc_record_replay.py ./phillip.csv 9999 replay localhost &
