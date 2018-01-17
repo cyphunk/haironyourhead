eaa001b0188395ec3948986b33e19277fbe24420
Nathan Fain <cyphunk@gmail.com>
2018-01-17 17:59:04 +0100

    .

--- server/run_ubuntu_login.sh
+++ server/run_ubuntu_login.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 
 cd ~/haironyourhead/server
-DISPLAY=:0 gnome-terminal -x bash -c 'node server.js --runtestserver --interface enp3s0 --showname runonlogin; bash'
+DISPLAY=:0 gnome-terminal -x bash -c 'node server.js --runtestserver --interface enp3s0 --showname runonlogin; bash' &
+sleep 4
+DISPLAY=:0 firefox http://localhost:8081/graphs &
